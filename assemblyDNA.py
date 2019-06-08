40ac06639ac5079f0727a8dad6a471ccf48cbf39
root <root@DESKTOP-6GQ5HBB.localdomain>
2019-06-09 04:57:13 +0900

    String match with bruteforce

---
+++ assemblyDNA.py
@@ -0,0 +1,69 @@
+def bruteforce(shortread, originDNA, missmatch=3):
+    originLength = len(originDNA)
+    shortreadLength = len(shortread)
+
+    for i in range(0, originLength - shortreadLength):
+        _missmatch = 0
+        isMathched = True
+        for j in range(0, len(shortread)):
+            if originDNA[i+j] == shortread[j]:
+                continue
+            elif originDNA[i+j] != shortread[j] and _missmatch < missmatch:
+                _missmatch = _missmatch + 1
+            else:
+                isMathched = False
+                break
+
+        if isMathched:
+            return i
+
+    return None
+
+def readShortSequence(filename):
+    file = open(filename, 'r')
+    shortreads = file.read().splitlines()
+    return shortreads
+
+def readOriginDNA(filename):
+    file = open(filename, 'r')
+    originDNA = file.read()
+
+    return originDNA
+
+def assembly(shortreads, originDNA, getPosition=bruteforce):
+    assembly = '-' * len(originDNA)
+
+    for shortread in shortreads:
+        length = len(shortread)
+        index = getPosition(shortread, originDNA)
+        if index:
+            assembly = assembly[:index] + shortread + assembly[index + length:]
+
+    return assembly
+
+    
+def writeDNA(assembly, filename):
+    file = open(filename, 'w')
+    file.write(assembly)
+
+def compareDNA(originDNA, assemblyDNA):
+    originLength = len(originDNA)
+    assemblyLength = len(assemblyDNA)
+
+    matchCount = 0
+
+    for i in range(0, originLength):
+        if originDNA[i] == assemblyDNA[i]:
+            matchCount = matchCount + 1
+
+    print("origin length : " + str(originLength))
+    print("aseembly legnth : " + str(assemblyLength))
+    print("matched count : " + str(matchCount))
+    print("match rate : " + str(matchCount / originLength))
+
+if __name__ == "__main__":
+    shortreads = readShortSequence("short_read.txt")
+    originDNA = readOriginDNA("origin_dna.txt")
+    assemblyDNA = assembly(shortreads, originDNA)
+    writeDNA(assemblyDNA, "assembly_dna.txt")
+    compareDNA(originDNA, assemblyDNA)
